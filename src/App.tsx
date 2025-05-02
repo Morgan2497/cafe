7cbcd210ad561b51de04182b541d45c6b13ae915
morgankim <morgan022497@gmail.com>
2025-05-01 22:27:58 -0700

    Initial commit: Basic React App with MainPage

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+// src/App.tsx
+import React from 'react';
+import MainPage from './pages/MainPage';
+import './App.css'; // You might have some global styles here
+
+function App() {
+  return (
+    <div>
+      <MainPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,14 @@
+// src/pages/MainPage/MainPage.tsx
+import React from 'react';
+
+const MainPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to Our Cafeteria!</h1>
+      <p>Browse our delicious menu and place your order.</p>
+      {/* More content will go here */}
+    </div>
+  );
+};
+
+export default MainPage;
